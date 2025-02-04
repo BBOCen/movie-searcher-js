c48a357b8f9c50edbe059985b58359b792b473da
BBOCen <158281421+BBOCen@users.noreply.github.com>
2025-02-04 11:48:48 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,35 +1,50 @@
-require('dotenv').config();
-const express = require('express');
-const fetch = require('node-fetch');
-const cors = require('cors');
+import 'dotenv/config';
+import express from 'express';
+import fetch from 'node-fetch';
+import cors from 'cors';
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));  // This will serve files from the 'public' folder
-
-
 app.get('/', (req, res) => {
     res.send('Hello from the Express server!');
 });
 
+app.get('/movie/:movie_id/videos', async (req, res) => {
+    const movie_id = req.params.movie_id;
+    const url = `https://api.themoviedb.org/3/movie/${movie_id}/videos`;
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+        }
+    };
 
-app.get('/movies', async (req, res) => {
     try {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}`);
+        const response = await fetch(url, options);
         const data = await response.json();
         res.json(data);
     } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch data' });
+        res.status(500).json({ error: 'Failed to fetch trailer videos' });
     }
 });
 
 app.get('/search', async (req, res) => {
     const query = req.query.query;
+    const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+        }
+    };
+
     try {
-        const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1&api_key=${process.env.TMDB_API_KEY}`);
+        const response = await fetch(url, options);
         const data = await response.json();
         res.json(data);
     } catch (error) {
